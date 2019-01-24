992173fa1208db67f9557ba93c2d8315b9b9b99e
Paul Bicknell <bicknell.paul@virgin.net>
2019-01-24 14:57:47 +0000

    Update to log in and books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
+  def index
+	@books = Book.all
+  end
+  def show
+	@book = Book.find(params[:id])
+  end
   def new
   end
+  def create
+	@book = Book.new(book_params)
+	
+	@book.save
+	redirect_to @book
+  end
+  def update
+	@book = Book.find(params[:id])
+	
+	if @book.update(book_params)
+		redirect_to @book
+	else
+		render 'edit'
+	end
+  end
+  
+  private
+	def book_params
+		params.require(:book).permit(:title, :author, :isbn)
+	end
 end
